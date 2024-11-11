b20de33689e290fda95935a11e3e00f6d4193298
Ubuntu <ubuntu@ip-172-31-23-142.us-west-1.compute.internal>
2024-11-11 20:06:15 +0000

    Backend-Global

--- server.js
+++ server.js
@@ -3,6 +3,41 @@ require('dotenv').config(); // Carga las variables de entorno
 const app = require('./app'); // Importa la aplicación desde app.js
 const sequelize = require('./config/sequelize'); // Importa la conexión a la base de datos
 const PORT = process.env.PORT || 5000;
+const cors = require('cors');
+
+// Lista de orígenes permitidos
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://localhost:5000',  // Agrega este origen para solicitudes desde localhost:5000
+  'http://54.177.153.3',
+  'http://54.177.153.3:5000',
+  'http://54.177.153.3:3000' // Agrega el nuevo origen aquí
+];
+
+// Opciones de CORS
+const corsOptions = {
+  origin: (origin, callback) => {
+    console.log('Origen de la solicitud:', origin); // Verifica el origen de la solicitud
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      console.error('Origen no permitido:', origin);
+      callback(new Error('Origen no permitido por CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Incluye OPTIONS
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+};
+
+// Middleware global para CORS
+app.use(cors(corsOptions));
+
+// Middleware para solicitudes OPTIONS específicamente
+app.options('*', cors(corsOptions));
+
+
+
 
 // Sincroniza la base de datos antes de iniciar el servidor
 sequelize.sync()
